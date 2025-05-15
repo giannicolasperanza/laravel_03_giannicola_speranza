a934cb251d9deaf74b380f18cee63f3ef71be04d
Giannicola Speranza <speranzagiannicola@gmail.com>
2025-05-15 17:40:51 +0200

    aggiustato homepage in ./

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GeneralController;
 use App\Http\Controllers\ArticleController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 
 // Homepage
-Route::get('/homepage' , [GeneralController::class, 'homepage'])->name('homepage');
+Route::get('/' , [GeneralController::class, 'homepage'])->name('homepage');
 
 // Indice con tutti gli Articoli 
 Route::get('/articles',[ArticleController::class, 'articles'])->name('listArticles');
